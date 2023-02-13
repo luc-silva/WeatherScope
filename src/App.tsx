3ec0a8e6c2a0087edce2c9e75536b00f262c4210
Lucas Santos <lucas.silva1203@outlook.com>
2023-02-13 19:52:56 -0300

    Modify design and add temperature converter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
 import "./App.css";
-import { AppNavigator } from "./components/AppNavigator";
+import { useState, useEffect } from "react";
+import { AppHeader } from "./components/AppHeader";
 import { SettingsMenu } from "./components/SettingsMenu";
 import { WeatherMenu } from "./components/WeatherMenu";
 
 function App() {
+    let image = require("./img/clear.jpg");
+    let [actualTemperature, setActualTemperature] = useState(40);
+    let [isCelsius, toggleUnit] = useState(true);
+    
+
+    useEffect(() => {
+        if (isCelsius) {
+            let celsius = (actualTemperature - 32) * (5 / 9);
+            setActualTemperature(celsius);
+        } else {
+            let fahrenheit = actualTemperature * (9 / 5) + 32;
+            setActualTemperature(fahrenheit);
+        }
+    }, [isCelsius]);
+
     return (
-        <div className="App">
-            <div className={"weather-app-image"}>
-                <span className={"weather-tip"}>
+        <div className="WeatherApp">
+            <div className={"weather-app-main-display"}>
+                <span className={"display-tip"}>
                     Click above the temperature to change its format
                 </span>
-                <span className={"weather-display"}>
-                    <div className={"weather-city"}>----</div>
+                <div className={"display-image"}>
+                    <img src={image} />
+                </div>
+                <div className={"display-info"}>
+                    <div className={"weather-city"}>London</div>
                     <div className={"weather-status-display"}>
-                        <div className={"weather-status"}>----</div>
-                        <div className={"weather-temperature"}>N/A°C</div>
+                        <div className={"weather-status"}>Clear Skies</div>
+                        <div
+                            className={"weather-temperature"}
+                            onClick={() => {
+                                toggleUnit(!isCelsius);
+                            }}
+                        >
+                            {(isCelsius &&
+                                `${actualTemperature.toFixed(2)} C`) ||
+                                `${actualTemperature.toFixed(2)} F`}
+                        </div>
                     </div>
-                </span>
-            </div>   
-			{/* <AppNavigator />
-			<SettingsMenu />
+                </div>
+            </div>
+            <AppHeader />
+            {/*<SettingsMenu />
 			<WeatherMenu />          */}
         </div>
     );
