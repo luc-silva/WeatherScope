2881824784a372f208d95bf753b1f5d2ed709823
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-28 15:57:08 -0300

    Modify WeatherCard
    WeatherCard is now resposible for loading its own city weather data

--- src/components/cards/WeatherCard.tsx
+++ src/components/cards/WeatherCard.tsx
@@ -1,30 +1,39 @@
+import { useEffect, useState } from "react";
 import {
     calculateKelvinToCelsius,
     calculateKelvinToFahrenheit,
 } from "../../utils/tools";
 import styles from "./WeatherCard.module.css";
+import { initialWeatherData } from "../../constants/defaultData";
+import WeatherService from "../../services/WeatherService";
 
-export const WeatherCard = ({ data }: { data: IWeatherData }) => {
+export const WeatherCard = ({ city }: { city: string }) => {
+    let [weatherData, setWeatherData] = useState(initialWeatherData);
+    useEffect(() => {
+        if (city) {
+            WeatherService.getWeatherStatusFromCity(city).then(setWeatherData);
+        }
+    });
     return (
         <div className={styles["weather-card"]}>
             <div className={styles["weather-card__city"]}>
-                <h3>{data.name}</h3>
-                <strong>{data.sys.country}</strong>
+                <h3>{weatherData.name}</h3>
+                <strong>{weatherData.sys.country}</strong>
             </div>
             <div className={styles["weather-card__main"]}>
                 <div className={styles["weather-card__status"]}>
                     <span>{`${calculateKelvinToFahrenheit(
-                        data.main.temp
+                        weatherData.main.temp
                     ).toFixed(2)}F`}</span>
-                    <span>{`${calculateKelvinToCelsius(data.main.temp).toFixed(
-                        2
-                    )}C`}</span>
+                    <span>{`${calculateKelvinToCelsius(
+                        weatherData.main.temp
+                    ).toFixed(2)}C`}</span>
                 </div>
                 <div className={styles["weather-card__condition"]}>
-                    <p>{data.weather[0].description}</p>
+                    <p>{weatherData.weather[0].description}</p>
                     <div>
                         <img
-                            src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                            src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
                         />
                     </div>
                 </div>
