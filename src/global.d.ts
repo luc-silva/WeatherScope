6d2152038602e9063a18530f733f9a970282a51d
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-28 10:40:11 -0300

    Add types for jpg extension

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,9 @@
-declare module '*.module.css' {
+declare module "*.module.css" {
     const classes: { [key: string]: string };
     export default classes;
-  }
+}
+
+declare module "*.jpg" {
+    const value: any;
+    export default value;
+}
