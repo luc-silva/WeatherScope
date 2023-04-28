53240b1641438c3d7543d6d833ddbe90de4de022
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-28 15:00:52 -0300

    Fix WeatherCard design and assigned data

--- src/components/cards/WeatherCard.tsx
+++ src/components/cards/WeatherCard.tsx
@@ -1,17 +1,33 @@
+import {
+    calculateKelvinToCelisuis,
+    calculateKelvinToFahrenheit,
+} from "../../utils/tools";
 import styles from "./WeatherCard.module.css";
 
 export const WeatherCard = ({ data }: { data: IWeatherData }) => {
     return (
         <div className={styles["weather-card"]}>
-            <span className={styles["weather-card-city"]}></span>
-            <div className={styles["weather-card-status-container"]}>
-                <div className={styles["weather-card-status"]}>
-                    <span>12F</span>
-                    <span>24C</span>
-                </div>
+            <div className={styles["weather-card__city"]}>
+                <h3>{data.name}</h3>
+                <strong>{data.sys.country}</strong>
             </div>
-            <div className={styles["weather-card-condition"]}>
-                Clear Sky *logo*
+            <div className={styles["weather-card__main"]}>
+                <div className={styles["weather-card__status"]}>
+                    <span>{`${calculateKelvinToFahrenheit(
+                        data.main.temp
+                    ).toFixed(2)}F`}</span>
+                    <span>{`${calculateKelvinToCelisuis(data.main.temp).toFixed(
+                        2
+                    )}C`}</span>
+                </div>
+                <div className={styles["weather-card__condition"]}>
+                    <p>{data.weather[0].description}</p>
+                    <div>
+                        <img
+                            src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     );
