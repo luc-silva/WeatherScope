dd97d13e923285d7dfa7fcaa27a10831e5560118
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-28 12:11:00 -0300

    Add WeatherService

---
+++ src/services/WeatherService.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+class WeatherService {
+    private API_KEY = "APPID=67556afc5b803ebc6540b49ddb13c6a3";
+    private API_URL = `https://api.openweathermap.org/data/2.5/weather?${this.API_KEY}`;
+    private UNIT_PARAM = "&units=";
+    private CITY_PARAM = "&q=";
+
+    public async getWeatherStatusFromCity(unit: string, city: string) {
+        return await axios
+            .get(this.API_URL + this.UNIT_PARAM + unit + this.CITY_PARAM + city)
+            .then(({ data }) => {
+                return data;
+            });
+    }
+}
+
+export default new WeatherService();
