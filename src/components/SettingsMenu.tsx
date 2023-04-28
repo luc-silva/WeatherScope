a6215cb38ef3fa3ab04ca9331196c8e8500f49a0
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-28 16:25:33 -0300

    Modify SettingsMenu
    Now user can edit your own info

--- src/components/SettingsMenu.tsx
+++ src/components/SettingsMenu.tsx
@@ -1,21 +1,75 @@
-import styles from "./SettingsMenu.module.css"
+import { ChangeEvent, FormEvent, useState } from "react";
+import styles from "./SettingsMenu.module.css";
+import { defaultUserSettings } from "../constants/defaultData";
 
-export const SettingsMenu = () => {
+export const SettingsMenu = ({
+    user,
+    setUser,
+}: {
+    user: IUser;
+    setUser: Function;
+}) => {
+    let [form, setForm] = useState(user);
+
+    function handleChange(event: ChangeEvent<HTMLElement>) {
+        let target = event.target;
+        if (
+            target instanceof HTMLInputElement ||
+            target instanceof HTMLSelectElement
+        ) {
+            setForm({ ...user, [target.name]: target.value });
+        }
+    }
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        setUser(form);
+    }
     return (
-        <div className={styles["settings-container"]}>
-            <h2>Settings</h2>
-            <div>
-                default city
-                <input type="text" />
+        <section className={styles["settings"]}>
+            <div className={styles["settings__title"]}>
+                <h2>Settings</h2>
             </div>
-            <div>
-                unit
-                <select>
-                    <option value="">metric</option>
-                    <option value="">imperial</option>
-                </select>
+            <div className={styles["settings__form__container"]}>
+                <form
+                    action="POST"
+                    className={styles["settings__form"]}
+                    onSubmit={handleSubmit}
+                >
+                    <div className={styles["input-container"]}>
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            name="main-city"
+                            value={form.username}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className={styles["input-container"]}>
+                        <label htmlFor="main__city">Default City</label>
+                        <input
+                            type="text"
+                            name="main__city"
+                            value={form.main__city}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className={styles["input-container"]}>
+                        <label htmlFor="scale">Scale</label>
+                        <select
+                            name="scale"
+                            value={form.scale}
+                            onChange={handleChange}
+                        >
+                            <option value="metric">metric</option>
+                            <option value="imperial">imperial</option>
+                        </select>
+                    </div>
+                    <div className={styles["input-container"]}>
+                        <input type="submit" value="Submit Changes" />
+                    </div>
+                </form>
             </div>
-            <button>Submit Changes</button>
-        </div>
+        </section>
     );
 };
