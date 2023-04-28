02be3f7332698f535b2d221b267c5fea9de311c9
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-28 14:48:33 -0300

    Add new convertion operations

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,16 +1,54 @@
 /**
- * Calculates the celsius with given number.
+ * Converts given number in  fahrenheit to celsius.
  * @param value numeric value.
  * @returns Returns the result of the calculation
  */
-export function calculateCelsius(value: number): number {
+export function calculateFahrenheitToCelsius(value: number): number {
     return (value - 32) * (5 / 9);
 }
+
 /**
- * Calculates the fahrenheit with given number.
+ * Converts given number in celsius to fahrenheit.
  * @param value numeric value.
  * @returns Returns the result of the calculation
  */
-export function calculateFahrenheit(value: number): number {
+export function calculateCelsiusToFahrenheit(value: number): number {
     return value * (9 / 5) + 32;
+}
+
+
+/**
+ * Converts given number in  kelvin to celsius.
+ * @param value number to calculate.
+ * @returns Celsius respective value.
+ */
+export function calculateKelvinToCelisuis(value:number):number{
+    return value - 273.15
+}
+
+/**
+ * Converts given number in  celsius to kelvin.
+ * @param value number to calculate.
+ * @returns Kelvin respective value.
+ */
+export function calculateCelsiusToKelvin(value:number):number{
+    return value + 273.15
+}
+
+/**
+ * Converts given number in  kelvin to fahrenheit.
+ * @param value number to calculate.
+ * @returns Fahrenheit respective value.
+ */
+export function calculateKelvinToFahrenheit(value:number):number{
+    return (value - 273.15) * (9/5) + 32
+}
+
+/**
+ * Converts given number in  fahrenheit to kelvin.
+ * @param value number to calculate.
+ * @returns Kelvin respective value.
+ */
+export function calculateFahrenheitToKelvin(value:number):number{
+    return value + 273.15
 }
