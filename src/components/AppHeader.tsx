3d03454d22b1bf61c8786a98c3f1dffe3a1d6f58
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-29 15:11:49 -0300

    Modify header design

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -2,23 +2,37 @@ import { NavLink } from "react-router-dom";
 import styles from "./AppHeader.module.css";
 
 export const AppHeader = () => {
+    function setClass(prop: { isActive: boolean }) {
+        return prop.isActive
+            ? [styles["link-item"], styles["item--active"]].join(" ")
+            : styles["link-item"];
+    }
+
     return (
         <div className={styles["header"]}>
             <nav className={styles["header__navigation"]}>
                 <ul className={styles["header__navigation__links"]}>
                     <li>
-                        <NavLink to="weather">Weather</NavLink>
+                        <NavLink to="/weather" className={setClass}>
+                            Weather
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink to="news">News</NavLink>
+                        <NavLink to="/news" className={setClass}>
+                            News
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink to="agenda">Agenda</NavLink>
+                        <NavLink to="/agenda" className={setClass}>
+                            Agenda
+                        </NavLink>
                     </li>
                 </ul>
                 <div className={styles["header__navigation__extras"]}>
                     <div className={styles["settings-button"]}>
-                        <NavLink to="settings">Settings</NavLink>
+                        <NavLink to="/settings" className={setClass}>
+                            Settings
+                        </NavLink>
                     </div>
                 </div>
             </nav>
